f13342b132a37aacef79adeac3744d770cfc458a
strong6208 <hamilton6208@gmail.com>
2023-06-09 01:46:19 -0700

    implement the login check

--- controllers/auth.go
+++ controllers/auth.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"net/http"
 	"real-chat-backend/models"
+	"real-chat-backend/utils/token"
 
 	"github.com/gin-gonic/gin"
 )
@@ -65,4 +66,23 @@ func Register(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"message": "registration success"})
 
+}
+
+func CurrentUser(c *gin.Context) {
+
+	user_id, err := token.ExtractTokenID(c)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	u, err := models.GetUserByID(user_id)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "success", "data": u})
 }
